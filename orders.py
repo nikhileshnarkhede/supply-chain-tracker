a0ed706f543ae59547aacecdc420131058cb44a8
NIKHILESH NARKHEDE <65936667+nikhileshnarkhede@users.noreply.github.com>
2025-08-08 10:02:43 -0400

    Update orders.py

--- orders.py
+++ orders.py
@@ -3,7 +3,8 @@ import pandas as pd
 from pymongo import MongoClient
 from datetime import datetime
 
-client = MongoClient("mongodb://localhost:27017")
+# MongoDB client from secrets
+client = MongoClient(st.secrets["MONGO_URI"])
 db = client["supply_chain"]
 orders = db["orders"]
 products = db["products"]
